945b153b5a1d87f6cf15aa0f739d2e72d7de7811
calemaley <143690604+calemaley@users.noreply.github.com>
2024-07-04 09:17:06 +0300

    Delete my_ticket_system directory

--- my_ticket_system/events/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Event, Ticket, Review, Chat
-
-admin.site.register(Event)
-admin.site.register(Ticket)
-admin.site.register(Review)
-admin.site.register(Chat)
--- my_ticket_system/script/chat.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var chatBox = document.querySelector(".chat-box");
-    chatBox.scrollTop = chatBox.scrollHeight;
-});
