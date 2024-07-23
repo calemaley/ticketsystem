146e13e681df2377944edb2d623b525cb4d77040
calemaley <nephatnyaga1969@gmail.com>
2024-07-23 12:29:24 +0300

    main

--- events/urls.py
+++ events/urls.py
@@ -4,6 +4,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path('splash/',views.splash_page, name='splash_page'),
     path('', views.homepage, name='homepage'),
     path('noughty_by_nature/', views.noughty_by_nature_view, name='noughty_by_nature_view'),
     path('events/<int:event_id>/', views.event_detail, name='event_detail'),  
@@ -13,4 +14,5 @@ urlpatterns = [
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login'),
     path('logout/', views.logout_user, name='logout'),
+    
 ]
